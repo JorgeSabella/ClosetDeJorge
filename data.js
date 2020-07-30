78d45a7097e7fbdf33c1e35d35aa4d9a745d8aa6
jorgesabella <jorge_andres1996@hotmail.com>
2020-07-29 20:31:42 -0500

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010005",
+      "name": "R0010005",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.227251301253144,
+        "pitch": 0.27568166263618465,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.705271600208283,
+          "pitch": 0.28138529212684205,
+          "rotation": 0,
+          "target": "1-r0010004"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r0010004",
+      "name": "R0010004",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.06847060911670333,
+        "pitch": 0.11378462537870249,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.28942252035215965,
+          "pitch": 0.20463091520847954,
+          "rotation": 0,
+          "target": "0-r0010005"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
